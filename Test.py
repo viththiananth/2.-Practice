87012cf4b0d15f996475f218b4a13794973948b1
viththi <56210820+viththiananth@users.noreply.github.com>
2019-12-18 05:52:45 +0530

    Newly Added

--- Test.py
+++ Test.py
@@ -1,4 +1,7 @@
-import datetime
-now=datetime.datetime.now()
-print(now)
-print(now.strftime("%H:%M:%S %Y-%m-%d"))
+weight_kg = [81.65, 97.52, 95.25, 92.98, 86.18, 88.45]
+
+import numpy as np
+
+np_weight=np. array(weight_kg)
+
+print(np_weight)
