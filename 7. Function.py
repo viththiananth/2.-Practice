e21210fe13506ce8301d2df0d1444aa0ee4dcec2
viththi <56210820+viththiananth@users.noreply.github.com>
2020-01-13 06:42:26 +0530

    Newly Added

--- 7. Function.py
+++ 7. Function.py
@@ -46,3 +46,68 @@ def myfunc2(args):
     print(result)
 
 myfunc2('Viththi')
+
+
+def less_of_two_even(a,b):
+    if (a%2==0 and b%2==0):
+        print(min(a,b))
+    else:
+        print(max(a,b))
+
+less_of_two_even(5,4)
+
+
+def old_macdonald(name):
+    a = name[:3].capitalize()
+    b = name[3:].capitalize()
+    print(a+b)
+
+
+old_macdonald('macdonald')
+
+def master_yoda(text):
+    a=text.split()[::-1]
+    print(a)
+
+master_yoda('How are you')
+
+
+def almost_three(n):
+    print((abs(100-n)<=10)or (abs(200-n)<=10))
+
+almost_three(12)
+almost_three(92)
+almost_three(150)
+almost_three(209)
+
+
+
+def has_33(nums):
+    for i in range(0,len(nums)):
+        if nums[i:i+2]==[3,3]:
+            True
+        else:
+            False
+
+has_33(1,3,4,4,3,3)
+
+
+def paper_doll(string):
+    result=''
+    for i in string:
+        result+=i*3
+    print(result)
+
+
+
+paper_doll('viththi')
+
+def find_33(nums):
+    for x in range(0, len(nums)):
+        if (nums[x] == 3 and nums[x + 1] == 3):
+            print(True)
+        else:
+            print(False)
+
+
+find_33([1, 2, 3, 4, 4, 3, 3])
